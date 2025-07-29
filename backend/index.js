992f5f578df7d17503b63f17dac4f66ed1c53e7f
OmolloRovy <masimevanistelrooy@gmail.com>
2025-07-30 01:25:52 +0300

    Remove useUnifiedTopology option from MongoDB connection in db.js and index.js; add empty client.yml and server.yml files for GitHub Actions workflows

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  //useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.log(err)); 
