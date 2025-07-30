9d00484a8b98a32bdab5181ac53463da4cd064d8
OmolloRovy <masimevanistelrooy@gmail.com>
2025-07-30 07:28:58 +0300

    refactor: update server deployment workflow and simplify mongoose connection

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,6 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.log(err)); 
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
